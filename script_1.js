65113ef0a94fcfa675e63b78d574cde49163aace
Alexandre, Kelifa <alexandre.kelifa@hotmail.com>
2020-05-22 00:51:15 +0200

    commit hello name

---
+++ script_1.js
@@ -0,0 +1,6 @@
+newFunction();
+
+function newFunction() {
+  let name = prompt('Hello there :-) !!!! What is you little name?');
+  console.log(`Bonjour ${name}`);
+}
---
+++ script_2.js
@@ -0,0 +1,12 @@
+// Un prompt s'affiche avec la question suivante
+question = prompt("De quel nombre veux-tu calculer la factorielle?");
+
+// Utilisateur rentre un nombre (par ex 4)
+fonctionFactorielle = (number * number-1);
+
+// Dans la console le résultat est affiché
+for(let count = number) {
+  let number = prompt('De quel nombre veux-tu calculer la factorielle?');
+  number = (number * (number-1));
+  console.log(number `Le résultat est ${fonctionFactorielle}`);
+}
